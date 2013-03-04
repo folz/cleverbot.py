43e6c95fe89698d5eee5bb4015cb095096eebba2
Rodney Folz <folz@rodneyfolz.com>
2013-03-03 23:27:30 -0800

    Restructure to be a Python package.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'cleverbot',
+    packages = ['cleverbot'],
+    version = '0.1.0',
+    author = 'Rodney Folz',
+    author_email = 'folz@rodneyfolz.com',
+    url = 'https://github.com/folz/cleverbot.py',
+    description = 'An API for Cleverbot in Python',
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent'
+    ]
+)
