a9f5ee8a43221e3bceeeea8cf8fd4367dee778ce
AkashMalhotra <raz0rstr@hotmail.com>
2015-05-31 23:32:46 -0400

    voice recognition

---
+++ Main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import traceback
+import speech_recognition as sr
+import cleverbot
+import sys
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:  # audio source microphone ( computer default)
+
+
+    def main():
+        bot = cleverbot.Cleverbot()
+
+        while True:
+            audio = r.listen(source)
+            question = r.recognize(audio)
+            if question =='exit' or question=='Exit':
+                print('Goodbye')
+                sys.exit()
+
+
+
+            print 'Me:' + question
+            answer = bot.ask(question)
+            print ' Cleverbot: {}'.format(answer)
+
+
+
+
+
+    if __name__ == '__main__':
+        try:
+            main()
+        except Exception, err:
+
+            print traceback.format_exc(err)
