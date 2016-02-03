baadc019041798ac935f3657e3c39b78fff9ded5
Rodney Folz <rodney@rodneyfolz.com>
2016-02-02 17:35:10 -0800

    Add Python 3 language classifier to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name = 'cleverbot',
-    version = '0.2.0',
-    packages = ['cleverbot'],
-    install_requires = ['future', 'requests'],
-    tests_require = ['pytest'],
-    author = 'Rodney Folz',
-    author_email = 'folz@rodneyfolz.com',
-    url = 'https://github.com/folz/cleverbot.py',
-    description = 'An API for Cleverbot in Python',
-    classifiers = [
+    name='cleverbot',
+    version='0.2.1',
+    packages=['cleverbot'],
+    install_requires=['future', 'requests'],
+    tests_require=['pytest'],
+    author='Rodney Folz',
+    author_email='folz@rodneyfolz.com',
+    url='https://github.com/folz/cleverbot.py',
+    description='An API for Cleverbot in Python',
+    classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
     ]
 )
