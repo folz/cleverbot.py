6aaafa9dbd0c91b6a96861ef21c66c5f3068796a
Rodney Folz <rodney@rodneyfolz.com>
2016-03-28 15:06:00 -0700

    Update setup.py (now model after Django's).

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ from distutils.core import setup
 setup(
     name='cleverbot',
     version='0.2.1',
+    url='https://github.com/folz/cleverbot.py',
+    author='Rodney Folz',
+    author_email='pypi@rodneyfolz.com',
     description='An unofficial library to access the Cleverbot API',
+    license='BSD-2-Clause',
     packages=['cleverbot'],
     install_requires=['future', 'requests'],
     tests_require=['pytest'],
-    author='Rodney Folz',
-    author_email='folz@rodneyfolz.com',
-    url='https://github.com/folz/cleverbot.py',
     classifiers=[
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent'
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
