4d779436a3a519a8cbe2ccef5aecac50547e236e
Rodney Folz <rodney@rodneyfolz.com>
2016-03-28 15:06:15 -0700

    Add Python 3.5 tox tests and classifier.

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
